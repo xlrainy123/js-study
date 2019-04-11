033dbb6556c9b8b1c298cbe27fe662717514ef41
xlrainy123 <2414592951@qq.com>
2019-04-11 15:10:58 +0800

    添加create-react-app

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,17 @@
+import React , {Component} from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+
+class App extends Component{
+    render() {
+        return(
+            <div className="App">
+                <h1>
+                    Hello,World!
+                </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
